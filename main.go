ebf1dba764a0d5ec5f1b29e121d526b039609294
Ubuntu <ubuntu@ip-172-31-37-60.ap-northeast-1.compute.internal>
2021-10-26 15:03:53 +0000

    update

--- main.go
+++ main.go
@@ -7,19 +7,32 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var (
+	username  string = "root"
+	password  string = "password"
+	addr      string = "127.0.0.1"
+	port      int    = 3306
+	database  string = "db01"
+	parseTime bool   = false // time.time or string
+)
+
 type Product struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Price int    `json:"price"`
-	Image string `json:"image"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Price    int    `json:"age"`
+	Image    string `json:"image"`
+	CreateAt string `json:"createAt"`
+	UpdateAt string `json:"updateAt"`
 }
 
-func create(conn *sql.DB) error {
+func createTable(conn *sql.DB) error {
 	sql := `CREATE TABlE table01(
 		id INT NOT NULL AUTO_INCREMENT,
-		name VARCHAR(16) NOT NULL,
-		price INT,
-		image VARCHAR(64),
+		name VARCHAR(16) NOT NULL DEFAULT "",
+		price INT DEFAULT 0,
+		image VARCHAR(64) DEFAULT "",
+		created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 		PRIMARY KEY (id)
 	);`
 	_, err := conn.Query(sql)
@@ -29,12 +42,28 @@ func create(conn *sql.DB) error {
 	return nil
 }
 
-func insert(conn *sql.DB, command string) error {
-	_, err := conn.Query(command) //("INSERT INTO user_info (name, age) VALUES (?, ?)","syhlion",18,)
+func insert(conn *sql.DB, name string, price int, image string) error {
+	//"INSERT INTO table01(name, price) VALUES ('iphone12_64g', 26900)"
+	_, err := conn.Exec("INSERT INTO table01(name, price, image) VALUES (?, ?, ?)", name, price, image) //("INSERT INTO user_info (name, age) VALUES (?, ?)","syhlion",18,)
 	if err != nil {
 		return err
 	}
-	//defer res.Close()
+	return nil
+}
+
+func update(conn *sql.DB, id int, name string, price int, image string) error {
+	_, err := conn.Exec("UPDATE table01 SET name=?, price=?, image=? WHERE id=?", name, price, image, id) //("INSERT INTO user_info (name, age) VALUES (?, ?)","syhlion",18,)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func delete(conn *sql.DB, id int) error {
+	_, err := conn.Exec("DELETE FROM table01 WHERE id=?", id)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -47,19 +76,13 @@ func sqlSelect(conn *sql.DB, command string) error {
 
 	var products []Product
 	for res.Next() {
-		var (
-			id    int
-			name  string
-			price sql.NullInt64
-			image sql.NullString
-		)
-		err = res.Scan(&id, &name, &price, &image)
+		var product Product
+		err = res.Scan(&product.ID, &product.Name, &product.Price, &product.Image, &product.CreateAt, &product.UpdateAt)
 		if err != nil {
 			return err
 		}
-		var product = Product{id, name, int(price.Int64), image.String}
 		products = append(products, product)
-		fmt.Println(product.Name, product.Price)
+		//fmt.Println(product.Name, product.Price)
 	}
 	fmt.Println(products)
 	return nil
@@ -67,17 +90,31 @@ func sqlSelect(conn *sql.DB, command string) error {
 
 func main() {
 	// connect to mariaDB
-	conn, err := sql.Open("mysql", "root:951219@tcp(localhost:3306)/db01")
+	connInfo := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=%v", username, password, addr, port, database, parseTime)
+	conn, err := sql.Open("mysql", connInfo)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer conn.Close()
 
-	err = create(conn)
+	// err = createTable(conn)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	// err = insert(conn, "INSERT INTO table01(name, price) VALUES ('iphone12_64g', 26900)")
+	// err = insert(conn, "iphone11", 30000, "")
 	// if err != nil {
 	// 	fmt.Println(err)
+	// }
+
+	// err = update(conn, 2, "iphone11", 3000, "")
+	// if err != nil{
+	// 	log.Println(err)
+	// }
+
+	// err = delete(conn, 1)
+	// if err != nil {
+	// 	log.Println(err)
 	// }
 
 	err = sqlSelect(conn, "SELECT * FROM table01")
